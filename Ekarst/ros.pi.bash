3eef4649a4ede062fde15990cd9e5c85525b7770
Ubuntu <ubuntu@PI3A-ubuntu-18.04.2-armhf>
2019-02-24 17:50:50 +0000

    Add setup for the PI side of Ekarst

---
+++ Ekarst/ros.pi.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#PI3A-ubuntu-18.04.2-armhf
+source /opt/ros/melodic/setup.bash
+export PI=192.168.1.23
+export ROS_MASTER_URI=http://$PI:11311
+export ROS_IP=$PI
+export ROS_HOSTNAME=$PI
+
